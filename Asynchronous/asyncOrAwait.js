44214d5d8819f12bd4d0bf75106ef8ea871b3644
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2023-07-05 18:41:02 +0700

    Belajar asynchronous

--- Asynchronous/asyncOrAwait.js
+++ Asynchronous/asyncOrAwait.js
@@ -16,20 +16,21 @@
 // }
 let condition = true;
 let tesAsyncAwait = async (condition) => {
-    if(condition) {
+    if (condition) {
         return "Condition is fulfilled!";
-    }else{
+    } else {
         throw "Condition is rejected";
     }
 }
-//Membuat fungsi run menjadi asynchronous menggunakan async/await
-const run = async(condition) => {
-    try{
+// Membuat fungsi run menjadi asynchronous menggunakan async/await
+const run = async (condition) => {
+    try {
         const message = await tesAsyncAwait(condition);
-        console.log(message);
-        console.log("After condition is fulfilled");
-    }catch (error){
+        console.log(message);  // Output: Condition is fulfilled!
+        console.log("After condition is fulfilled"); // Output: After condition is fulfilled
+    } catch (error) {
         console.log(error);
     }
-}
+};
+
 run(true);
