2c9d413ab08a3f549111c4de54fb81a98e6435da
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2023-07-08 13:11:22 +0700

    Belajar web storage

---
+++ Local Storage/deleteSessionStorageData.js
@@ -0,0 +1,30 @@
+let cartList = [];
+function addToCart(name, qty){
+    const indexItem = cartList.findIndex(data => data.name === name);// memeriksa apakah item name sudah ada atau belum pada cartList
+    if(indexItem > -1){
+        cartList[indexItem].qty += 1; // jika sudah ada,  qty+1 pada data di index ke indexItem; ingat kembali materi array object
+    }else{ // jika belum ada, push data baru ke dalam cartList
+        cartList.push({
+            name,
+            qty
+        });
+    }
+    sessionStorage.setItem("carts", JSON.stringify(cartList));
+}
+
+function removeFromCart(name){
+    const indexItem = cartList.findIndex(data => data.name === name);
+    if(indexItem > -1){
+        if(cartList[indexItem].qty > 1){
+            cartList[indexItem].qty -= 1;
+        }else{
+            cartList.splice(indexItem, 1);
+        }
+    };
+    sessionStorage.setItem("carts", JSON.stringify(cartList));
+}
+
+function emptyCart(){
+    sessionStorage.removeItem('carts');
+    cartList = [];
+}
